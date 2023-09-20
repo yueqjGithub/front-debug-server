3f572029fe0e820d42ec1d1c1822ec08d78b5c15
岳全江 <410281036@qq.com>
2023-09-20 13:35:14 +0800

    init

---
+++ src/main/java/com/example/front_debug_server/FrontDebugServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.front_debug_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontDebugServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontDebugServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/front_debug_server/FrontDebugServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.front_debug_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontDebugServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
